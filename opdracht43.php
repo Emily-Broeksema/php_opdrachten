339c62fbe62830cf8ba358d667e913b932a23e5f
Emily-Broeksema <9017781@student.zadkine.nl>
2022-11-25 16:18:21 +0100

    opdr 4

--- opdracht43.php
+++ opdracht43.php
@@ -5,11 +5,11 @@ $var2 = 15;
 
  if($var1 > $var2){
     $var1 = 100 * 2 + 15;
-    echo $var1
+    echo $var1 ;
 } 
 else {
     $var2 = 15 * 2 + 10;
-    echo $var1
+    echo $var1 ;
 }
 
 ?>
--- opdracht44.php
+++ opdracht44.php
@@ -8,13 +8,18 @@ $software = 40;
 if($laptop > 150) {
     $som = $laptop * 1.19 ;
     echo "De oude pris is: &euro; $laptop,-
-    </br> Na verhoging van 19% is de prijs: &euro; $som,-"
+    </br> Na verhoging van 19% is de prijs: &euro; $som,-" ;
 }
 
-/* 11% duurder
 else if($software < 55) {
-    code
+    $som = $software * 1.11 ;
+    echo "De oude prijs is: $euro; $laptop,-
+    </br> Na verhoging van 19% is de prijs: $euro; $son,-" ;
 }
-else if
-    code...
+
+else if($software > 55) {
+    $som = $software / 11 * 100 ;
+    echo "De nieuwe prijs" ;
+}
+    
 ?>
